165e663c9801bf4990774707313d0cb39919b0d2
kroleg <kr.oleg@gmail.com>
2021-08-26 00:11:00 +0300

    Add task 2

--- Task_1/script.js
+++ Task_1/script.js
@@ -1,17 +1,17 @@
-var board = document.getElementById('boardInner');
-var numContainer = document.getElementById('numberContainer');
-var letterContainer = document.getElementById('letterContainer');
-var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
-var numbers = ['1', '2', '3', '4', '5', '6', '7', '8'].reverse();
+let board = document.getElementById('boardInner');
+let numContainer = document.getElementById('numberContainer');
+let letterContainer = document.getElementById('letterContainer');
+let letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+let numbers = ['1', '2', '3', '4', '5', '6', '7', '8'].reverse();
 
-renderLabels = () => {
+const renderLabels = () => {
     letters.forEach((x, i) => {
-        var el = document.createElement('div');
+        let el = document.createElement('div');
         el.innerText = x;
         el.className = 'label';
         letterContainer.appendChild(el);
 
-        var el2 = document.createElement('div');
+        let el2 = document.createElement('div');
         el2.innerText = numbers[i];
         el2.className = 'label';
         numContainer.appendChild(el2);
@@ -22,10 +22,10 @@ const renderBoard = () => {
   
     renderLabels();
 
-    var change = false;
-    var color = 'white';
-    for (var i = 0; i < 64; ++i) {
-        var el = document.createElement('div');
+    let change = false;
+    let color = 'white';
+    for (let i = 0; i < 64; ++i) {
+        let el = document.createElement('div');
         el.className = 'square';
         change = i % 8 === 0 || i % 8 === NaN;
         color = change ? color : color === 'white' ? 'black' : 'white';
---
+++ Task_2/script.js
@@ -0,0 +1,57 @@
+let bas = {
+    basket: [
+        {
+            product: 'Milk', 
+            count: 5, 
+            price: 100
+        },
+        {
+            product: 'Croissant', 
+            count: 4, 
+            price: 70
+        }, 
+        {
+            product: 'Lemonade', 
+            count: 3, 
+            price: 80
+        },
+        {
+            product: 'Bubble gum', 
+            count: 7, 
+            price: 40
+        }
+    ],
+    basketPrice: function() {
+
+      let basketPrice = 0;
+    
+      for (let i = 0; i < this.basket.length; i++) {
+          basketPrice += this.basket[i].count * this.basket[i].price;
+      }
+      return basketPrice;
+    },
+    numberOfGoods: function() {
+        
+        let numberGoods = 0;
+    
+        for (let i = 0; i < this.basket.length; i++) {
+            numberGoods += this.basket[i].count;
+        }
+      return numberGoods;
+    }
+};
+
+let messageDiv = document.getElementById('message');
+let el = document.createElement('div');
+el.className = 'label';
+
+const nfg = bas.numberOfGoods();
+
+if ((bas.basket.length > 0) && (nfg > 0)) {
+    
+    el.innerText = 'В корзине ' + nfg + ' товаров на сумму ' + bas.basketPrice() + ' рублей';
+} else {
+    el.innerText = 'Корзина пуста!';
+}
+
+messageDiv.appendChild(el);
